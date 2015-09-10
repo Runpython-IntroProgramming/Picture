0edb132730abc5aec3bfa84d3316b33b4ad6d5f5
sarahdunbar <sarahdunbar@hanovernorwichschools.org>
2015-09-10 14:56:57 -0400

    Updated from Brython Server: 9/10/2015 2:56:57 PM

--- picture.py
+++ picture.py
@@ -27,6 +27,8 @@ for detailed information on ggame.
 from ggame import App, Color, LineStyle, Sprite, RectangleAsset, CircleAsset, EllipseAsset, PolygonAsset
 
 # add your code here \/  \/  \/
+reddark = Color(0xff0000, 1.0)
+redlight = Color(0xff0000, 0.5)
 bluedark = Color(0x0000ff, 1.0)
 bluelight = Color(0x0000ff, 0.5)
 greendark = Color(0x00ff00, 1.0)
@@ -38,6 +40,21 @@ white = Color(0x000000, 0.0)
 thinlineblack = LineStyle (1, black)
 thinlinegrey = LineStyle (1, grey)
 
+ocean = RectangleAsset (600, 1600, thinlineblack, bluelight)
+land = EllipseAsset (100, 75, thinlinegrey, greendark)
+house1 = RectangleAsset (50, 50, thinlineblack, bluedark)
+sailboat = PolygonAsset ([(0, 0), (50, 25), (0, 50), (0, 0)], thinlinegrey, white)
+roof1 = PolygonAsset ([(25, 0), (50, 50), (0, 50), (25, 0)], thinlinegrey, reddark)
+ship = PolygonAsset ([(0, 0), (100, 0), (75, 20), (25, 20), (0, 0)], thinlineblack, reddark)
+ship2 = PolygonAsset ([(0, 0), (100, 0), (75, 20), (25, 20), (0, 0)], thinlineblack, greenlight)
+
+Sprite (land, (650, 500))
+Sprite(ocean, (200, 500))
+Sprite(house1, (650, 400))
+Sprite(roof1, (650, 350))
+Sprite(ship, (400, 480))
+Sprite(ship2, (400, 480))
+Sprite(sailboat, (450, 430))
 
 
 # add your code here /\  /\  /\
