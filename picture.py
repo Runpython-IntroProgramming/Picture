713f2c0cbaa63bbd99ecb0c035cf7dcc11e2ef0e
ChubbyPotato <suhangui@hanovernorwichschools.org>
2015-09-11 18:07:45 -0400

    Updated from Brython Server: 9/11/2015 6:07:44 PM

--- picture.py
+++ picture.py
@@ -1,9 +1,9 @@
 """
 picture.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: <Suhan Gui>
+Credit: <http://www.color-hex.com/>
 
-Assignment:
+Assignment: Picture
 
 Use the ggame library to "paint" a graphical picture of something (e.g. a house, a face or landscape).
 
@@ -31,26 +31,49 @@ red = Color(0xff0000, 1.0)
 green = Color(0x0ff000, 1.0)
 blue = Color(0x0000ff, 1.0)
 black = Color(0x000000, 1.0)
-white= Color(100, 1.0)
+white= Color(0xfffffff, 1.0)
+gold= Color(0xffd700, 1.0)
+skin= Color(0xffefdb, 1.0)
+blue=
+pale=
 
 thinline = LineStyle(2, black)
 
-
-face= CircleAsset(300, thinline, green)
+face= CircleAsset(300, thinline, skin)
 Pupil= EllipseAsset(10,100,thinline,black)
 Mouth= EllipseAsset(100,30,thinline,red)
-ring1= CircleAsset(200, thinline, white)
+ring1= CircleAsset(50, thinline, white)
+ring2= CircleAsset(60, thinline, gold)
+face2= CircleAsset
+Geyes= EllipseAsset
+Gpupil= 
+Gmouth= 
+WierdEar= PolygonAsset
+Ground= RectangleAsset
 
 """ Frodo"""
 Sprite(face, (400,400))
-Sprite(Pupil, (600,400))
-Sprite(Pupil, (500,400))
+Sprite(Pupil, (600,350))
+Sprite(Pupil, (500,350))
 Sprite(Mouth, (550,600))
+Sprite(WierdEar,
 
 """ring"""
-Sprite(ring1, (400,400))
+Sprite(ring2, (900,150))
+Sprite(ring1, (900,150))
+
+"""gollum"""
+sprite(face2,
+sprite(Geyes,
+sprite(Geyes,
+sprite(Gpupil,
+sprite(Gpupil,
+sprite(Gmouth,
+
+sprite(Ground
 # add your code here /\  /\  /\
 
 
 myapp = App()
 myapp.run()
+
