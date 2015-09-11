a707772dd9a7026728bb5cef3f56c86d4f448024
jasminelou <yajielou@hanovernorwichschools.org>
2015-09-11 14:56:30 -0400

    Updated from Brython Server: 9/11/2015 2:56:30 PM

--- picture.py
+++ picture.py
@@ -1,7 +1,7 @@
 """
 picture.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: Jasmine Lou
+Credit: The internet, classmates
 
 Assignment:
 
@@ -26,8 +26,6 @@ for detailed information on ggame.
 """
 from ggame import App, Color, LineStyle, Sprite, RectangleAsset, CircleAsset, EllipseAsset, PolygonAsset
 
-from ggame import App, Color, LineStyle, Sprite
-from ggame import RectangleAsset, CircleAsset, EllipseAsset, PolygonAsset
 
 # Three primary colors with no transparency (alpha = 1.0)
 red = Color(0xff0000, 1.0)
@@ -35,23 +33,27 @@ green = Color(0x00ff00, 1.0)
 blue = Color(0x0000ff, 1.0)
 black = Color(0x000000, 1.0)
 seafoam = Color(0x58FAD0, 1.0)
+navy = Color(0x08088A, 1.0)
+gray = Color(0xA4A4A4, 1.0)
+darkgray = Color(0x585858, 1.0)
 
 # Define a line style that is a thin (1 pixel) wide black line
 thinline = LineStyle(1, black)
 mediumline = LineStyle(3, black)
 thickline = LineStyle(5, black)
 redline = LineStyle(3, red)
+noline = LineStyle(0, red)
 # A graphics asset that represents a rectangle
-rectangle = RectangleAsset(500, 200, mediumline, seafoam)
-circle = CircleAsset(30, redline, blue)
+rectangle = RectangleAsset(500, 200, noline, navy)
+circle = CircleAsset(30, noline, darkgray)
+road = RectangleAsset(2000, 100, noline, gray)
+cartriangle1 = PolygonAsset(
 
 # Now display a rectangle
 Sprite(rectangle, (100, 200))
 Sprite(circle, (500, 400))
 Sprite(circle, (200, 400))
-
-myapp = App()
-myapp.run()
+Sprite(road, (0, 430))
 
 
 myapp = App()
