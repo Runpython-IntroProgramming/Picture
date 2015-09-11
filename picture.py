36eb16f2f420faef374db7263aee7333faf1f2d6
ChubbyPotato <suhangui@hanovernorwichschools.org>
2015-09-11 15:40:15 -0400

    Updated from Brython Server: 9/11/2015 3:40:14 PM

--- picture.py
+++ picture.py
@@ -27,8 +27,28 @@ for detailed information on ggame.
 from ggame import App, Color, LineStyle, Sprite, RectangleAsset, CircleAsset, EllipseAsset, PolygonAsset
 
 # add your code here \/  \/  \/
+red = Color(0xff0000, 1.0)
+green = Color(0x0ff000, 1.0)
+blue = Color(0x0000ff, 1.0)
+black = Color(0x000000, 1.0)
+white= Color(100, 1.0)
+
+thinline = LineStyle(2, black)
 
 
+face= CircleAsset(300, thinline, green)
+Pupil= EllipseAsset(10,100,thinline,black)
+Mouth= EllipseAsset(100,30,thinline,red)
+ring1= CircleAsset(200, thinline, white)
+
+""" Frodo"""
+Sprite(face, (400,400))
+Sprite(Pupil, (600,400))
+Sprite(Pupil, (500,400))
+Sprite(Mouth, (550,600))
+
+"""ring"""
+Sprite(ring1, (400,400))
 # add your code here /\  /\  /\
 
 
