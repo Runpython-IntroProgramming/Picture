71e10fb979f2a0935bdfc5efe31fd6339327eb8d
sarahdunbar <sarahdunbar@hanovernorwichschools.org>
2015-09-11 14:32:15 -0400

    Updated from Brython Server: 9/11/2015 2:32:15 PM

--- picture.py
+++ picture.py
@@ -27,26 +27,31 @@ for detailed information on ggame.
 from ggame import App, Color, LineStyle, Sprite, RectangleAsset, CircleAsset, EllipseAsset, PolygonAsset
 
 # add your code here \/  \/  \/
-reddark = Color(0xff0000, 1.0)
-redlight = Color(0xff0000, 0.5)
-bluedark = Color(0x0000ff, 1.0)
-bluelight = Color(0x0000ff, 0.5)
-greendark = Color(0x00ff00, 1.0)
-greenlight = Color(0x00ff00, 0.5)
+makebrown = Color(0xff0000, 1.0)
+plum = Color(0xcc00ff, 1.0)
+water = Color(0x0000ff, 0.5)
+grassy = Color(0xeeff00, 1.0)
+makebrown2 = Color(0x00ff00, 0.5)
 black = Color(0x000000, 1.0)
 grey = Color(0x000000, 0.5)
 white = Color(0x000000, 0.0)
+brown = Color(0xA0522D, 1.0)
+skinwhite = Color(0xFFE7BA, 1.0)
+tunic = Color(0x872657, 1.0)
+fishscale = Color
 
 thinlineblack = LineStyle (1, black)
 thinlinegrey = LineStyle (1, grey)
 
-ocean = RectangleAsset (600, 1600, thinlineblack, bluelight)
-land = EllipseAsset (100, 75, thinlinegrey, greendark)
+ocean = RectangleAsset (600, 1600, thinlineblack, water)
+land = EllipseAsset (100, 75, thinlinegrey, grassy)
 house1 = RectangleAsset (50, 50, thinlineblack, bluedark)
 sailboat = PolygonAsset ([(0, 0), (50, 25), (0, 50), (0, 0)], thinlinegrey, white)
-roof1 = PolygonAsset ([(25, 0), (50, 50), (0, 50), (25, 0)], thinlinegrey, reddark)
-ship = PolygonAsset ([(0, 0), (100, 0), (75, 20), (25, 20), (0, 0)], thinlineblack, reddark)
-ship2 = PolygonAsset ([(0, 0), (100, 0), (75, 20), (25, 20), (0, 0)], thinlineblack, greenlight)
+roof1 = PolygonAsset ([(25, 0), (50, 50), (0, 50), (25, 0)], thinlinegrey, brown)
+ship = PolygonAsset ([(0, 0), (100, 0), (75, 20), (25, 20), (0, 0)], thinlineblack, makebrown)
+ship2 = PolygonAsset ([(0, 0), (100, 0), (75, 20), (25, 20), (0, 0)], thinlineblack, makebrown2)
+personhead = EllipseAsset (5, 5, thinlinegrey, skinwhite)
+personbody = RectangleAsset (10, 15, thinlineblack, tunic)
 
 Sprite (land, (650, 500))
 Sprite(ocean, (200, 500))
@@ -55,6 +60,8 @@ Sprite(roof1, (650, 350))
 Sprite(ship, (400, 480))
 Sprite(ship2, (400, 480))
 Sprite(sailboat, (450, 430))
+Sprite(personbody, (420, 465))
+Sprite(personhead, (425, 458))
 
 
 # add your code here /\  /\  /\
