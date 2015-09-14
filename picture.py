8254e56577c93423d19eaf2e0428c823d155f0ec
sawyerhanlon <sawyerhanlon@hanovernorwichschools.org>
2015-09-14 14:24:19 -0400

    Updated from Brython Server: September 14, 2015 2:24:18 PM EDT

--- picture.py
+++ picture.py
@@ -32,15 +32,24 @@ green = Color(0x00ff00, 1.0)
 blue = Color(0x0000ff, 1.0)
 black = Color(0x000000, 1.0)
 Sawyer = Color(0x9e0000, 1.0)
+yellow = Color(0Xffff00, 1.0)
+
 thinline=LineStyle(1, black)
+
 elipse=EllipseAsset(200, 75, thinline, Sawyer)
 Sprite(elipse, (600, 700))
+bouy=ElipseAsset(75, 14, thinline, red)
+Sprite(float, (80, 500))
 rectangle=RectangleAsset(100200, 500, thinline, blue)
 Sprite(rectangle, (10, 700))
 mast=RectangleAsset(15, 450, thinline, Sawyer)
 Sprite(mast, (600, 200))
-sail=PolygonAsset([(600,200), (850,150), (600,100), (600,200)], thinline, red)
+sail=PolygonAsset([(600,400), (850,300), (600,200), (600,400)], thinline, red)
 Sprite(sail, (0,0))
+sun=CircleAsset(80, thinline, yellow)
+Sprite(sun, (900, 20))
+float=ElipseAsset(75, 14, thinline, red)
+Sprite(bouy, 
 
 
 # add your code here /\  /\  /\
