9c52dc4007c2c374d2aaf8a191d0ed3714952dae
averywallis <averybwallis@gmail.com>
2016-01-22 14:56:33 -0500

    Update picture.py

--- picture.py
+++ picture.py
@@ -1,7 +1,7 @@
 """
 picture.py
 Author: Avery Wallis
-Credit: <list sources used, if any>
+Credit: None so far
 
 Assignment:
 
@@ -26,15 +26,16 @@ for detailed information on ggame.
 """
 from ggame import App, Color, LineStyle, Sprite, RectangleAsset, CircleAsset, EllipseAsset, PolygonAsset
 
-# add your code here \/  \/  \/
+
 red = Color(0xff0000, 1.0)
 green = Color(0x00ff00, 1.0)
 blue = Color(0x0000ff, 1.0)
 black = Color(0x000000, 1.0)
 
+thinline= LineStyle(1, black)
+thickline= LineStyle(5, black)
 
 
-# add your code here /\  /\  /\
 
 
 myapp = App()
