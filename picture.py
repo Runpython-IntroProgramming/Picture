e42ff6e2a22d2e51ad20f06b586a07e9446aa022
averywallis <averybwallis@gmail.com>
2016-01-25 14:24:10 -0500

    Updated from Brython Server: 1/25/2016 2:24:10 PM

--- picture.py
+++ picture.py
@@ -27,12 +27,13 @@ red = Color(0xff0000, 1.0)
 green = Color(0x00ff00, 1.0)
 blue = Color(0x0000ff, 1.0)
 black = Color(0x000000, 1.0)
+orange = Color(0xffa500, 1.0)
 
 thinline= LineStyle(1, black)
 thickline= LineStyle(5, black)
 thickishline= LineStyle(2.5, black)
 
-rectangle = RectangleAsset(50, 20, thinline, blue)
+rectangle = RectangleAsset(50, 20, thinline, orange)
 
 Sprite(rectangle, (100, 20))
 
