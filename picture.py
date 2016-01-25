d723f268d537b3e09169075983a8f8693a57652e
CANDYISLIFE <billybender@hanovernorwichschools.org>
2016-01-25 14:52:51 -0500

    Updated from Brython Server: 1/25/2016 2:52:51 PM

--- picture.py
+++ picture.py
@@ -40,7 +40,7 @@ black = Color(0x000000, 1.0)
 # Define a line style that is a thin (1 pixel) wide black line
 thinline = LineStyle(1, black)
 # A graphics asset that represents a rectangle
-rectangle = RectangleAsset(50, 20, thinline, blue)
+rectangle = RectangleAsset(200, 80, thinline, blue)
 
 # Now display a rectangle
 Sprite(rectangle, (800, 300))
