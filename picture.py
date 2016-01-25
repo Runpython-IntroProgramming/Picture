5a761e3fc0efaa9b166d192d8d0f8fb6accbdc3c
averywallis <averybwallis@gmail.com>
2016-01-25 14:32:27 -0500

    Updated from Brython Server: 1/25/2016 2:32:26 PM

--- picture.py
+++ picture.py
@@ -34,7 +34,7 @@ thickline= LineStyle(5, black)
 thickishline= LineStyle(2.5, black)
 
 rectangle = RectangleAsset(50, 20, thinline, orange)
-poly = PolygonAsset([(1,1), (20,2), (30,3), (40,4)], thinline, orange)
+poly = PolygonAsset([(0,0), (50,50), (50,100), (0,0)], thinline, orange)
 
 Sprite(rectangle, (100, 20))
 Sprite(poly, (10, 20))
