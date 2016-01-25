1eb2cd2ef9ded90718bec6335555f5fa6be5eec8
EthanAdner <ethanda99@gmail.com>
2016-01-25 14:46:52 -0500

    Updated from Brython Server: 1/25/2016 2:46:52 PM

--- picture.py
+++ picture.py
@@ -33,7 +33,7 @@ blue = Color(0x0000ff, 1.0)
 black = Color(0x000000, 1.0)
 
 thickline = LineStyle(1 , red)
-CircleAsset = (500, thickline, red)
+cirlce = CircleAsset(500, thickline, red)
 Sprite(CircleAsset, (500, 500)) 
 
 # add your code here /\  /\  /\
