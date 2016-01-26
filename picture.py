b34c90fcf6ab6328aaa856e2bc2e4792a60a08ba
adamglueck <adam.glueck1@gmail.com>
2016-01-26 14:26:36 -0500

    Updated from Brython Server: 1/26/2016 2:26:35 PM

--- picture.py
+++ picture.py
@@ -27,8 +27,23 @@ for detailed information on ggame.
 from ggame import App, Color, LineStyle, Sprite, RectangleAsset, CircleAsset, EllipseAsset, PolygonAsset
 
 # add your code here \/  \/  \/
-
-
+red = Color(0xff0000, 1.0)
+green = Color(0x00ff00, 1.0)
+blue = Color(0x00BFFF, 1.0)
+black = Color(0x000000, 1.0)
+yellow = Color(0xFFFF00, 1.0)
+green = Color(0x09FF00, 1.0)
+white = Color(0xFFFFFF, 1.0)
+thinlineblack = LineStyle(1, black)
+thinlinegreen = LineStyle(1, green)
+thinlineblue = LineStyle(1, blue)
+thinlineyellow = LineStyle(1, yellow)
+thinlinegreen = LineStyle(1, green)
+bluesky = RectangleAsset(1280, 720, thinlinegreen, blue)
+Sprite(bluesky)
+mountain =PolygonAsset([(0,720), (200,720,), (100,520)], thinlineblack, black)
+mountaincap =PolygonAsset([(50,620), (150,620,), (100,520)], thinlinewhite, white)
+Sprite(mountain)
 # add your code here /\  /\  /\
 
 
