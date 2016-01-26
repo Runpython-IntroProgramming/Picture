de96a348192061468c0220235e1179dc7d58497b
mcfey <maryfeyrer@hanovernorwichschools.org>
2016-01-26 14:19:09 -0500

    Updated from Brython Server: 1/26/2016 2:19:19 PM

--- picture.py
+++ picture.py
@@ -55,9 +55,10 @@ Sprite(tree2, (0, 90))
 Sprite(tree, (0, 45))
 Sprite(stem, (62, 186))
 Sprite(flower, (60, 182))
-Sprite(flower, (60, 172))
+Sprite(flower, (55, 172))
 Sprite(flower, (70, 182))
-Sprite(flower, (70, 172))
+Sprite(flower, (75, 172))
+Sprite(flower, (65, 165))
 
 # add your code here /\  /\  /\
 
