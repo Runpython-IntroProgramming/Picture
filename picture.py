55dda4c3de1a892a28449c9770e5b07e3b1689f8
averywallis <averybwallis@gmail.com>
2016-01-27 13:53:17 -0500

    Updated from Brython Server: 1/27/2016 1:53:17 PM

--- picture.py
+++ picture.py
@@ -45,14 +45,14 @@ portalline2=LineStyle(1, orange)
 wall=RectangleAsset(500,500, noline, wall)
 blueportal=EllipseAsset(27, 60, portalline, blue)
 orangeportal=EllipseAsset(27, 60, portalline2, orange)
-exit=CircleAsset(15, thinline,plat)
+exit=CircleAsset(15, noline, plat)
 start=RectangleAsset(200, 50, noline, plat)
 
 Sprite(wall, (400,20))
 Sprite(wall, (100,20))
 Sprite(blueportal, (200,200))
 Sprite(orangeportal, (300,100))
-Sprite(exit, (50,50))
+Sprite(exit, (500,100))
 Sprite(start, (100,400))
 
 
