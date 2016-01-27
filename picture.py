5f2f7ed28b4fc44e09507df18d0bf2eb28533b61
phstearns <phstearns13@gmail.com>
2016-01-27 13:17:44 -0500

    Update picture.py

--- picture.py
+++ picture.py
@@ -27,8 +27,11 @@ for detailed information on ggame.
 from ggame import App, Color, LineStyle, Sprite, RectangleAsset, CircleAsset, EllipseAsset, PolygonAsset
 
 # add your code here \/  \/  \/
-
-
+red = Color(0xff0000, 1.0)
+green = Color(0x00ff00, 1.0)
+blue = Color(0x0000ff, 1.0)
+black = Color(0x000000, 1.0)
+thinline = LineStyle(1, black)
 # add your code here /\  /\  /\
 
 
