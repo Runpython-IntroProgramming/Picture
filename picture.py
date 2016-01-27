46be1f4de1436b3564d3f064367ab18a7b07d7bf
averywallis <averybwallis@gmail.com>
2016-01-27 13:54:07 -0500

    Updated from Brython Server: 1/27/2016 1:54:07 PM

--- picture.py
+++ picture.py
@@ -52,7 +52,7 @@ Sprite(wall, (400,20))
 Sprite(wall, (100,20))
 Sprite(blueportal, (200,200))
 Sprite(orangeportal, (300,100))
-Sprite(exit, (500,100))
+Sprite(exit, (800,100))
 Sprite(start, (100,400))
 
 
