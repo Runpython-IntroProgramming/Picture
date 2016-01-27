6539130fc547ba2a870f8653ebe314c697d4cce3
davidwilson826 <davidwilson@hanovernorwichschools.org>
2016-01-27 13:41:55 -0500

    Updated from Brython Server: 1/27/2016 1:41:55 PM

--- picture.py
+++ picture.py
@@ -54,12 +54,14 @@ Ground = RectangleAsset(SkyWidth, GroundHeight, ThinLine, GroundCol)
 Sun = CircleAsset(SkyWidth/15, ThinLine, Yellow)
 Mount = PolygonAsset([(SkyWidth-(MountWidth/2)-OffSide, SkyHeight-MountHeight), (SkyWidth-MountWidth-OffSide, SkyHeight), (SkyWidth-OffSide, SkyHeight)], ThinLine, MountCol)
 Snow = PolygonAsset([(SnowWidth/2, 0), (0, SnowHeight), (SnowWidth, SnowHeight)], ThinLine, White)
+Cloud = EllipseAsset(SkyWidth/6, SkyHeight/20, ThinLine, White)
 
 Sprite(Back)
 Sprite(Ground, (0, SkyHeight))
 Sprite(Sun, (SkyWidth-3*OffSide, SkyHeight-MountHeight))
 Sprite(Mount)
 Sprite(Snow, (SkyWidth-(MountWidth/2)-(SnowWidth/2)-OffSide, SkyHeight-MountHeight))
+Sprite(Cloud)
 
 # add your code here /\  /\  /\
 
