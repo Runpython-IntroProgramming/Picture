528794e217f88783337312488703ecd08dc15606
glenpassow <glenpassow@hanovernorwichschools.org>
2016-01-29 14:31:56 -0500

    Updated from Brython Server: January 29, 2016 2:31:55 PM EST

--- picture.py
+++ picture.py
@@ -47,7 +47,10 @@ triangle = PolygonAsset([(50,50),(100,200),(0,80)], thinline, green)
 rectangle2 = RectangleAsset(100, 100, thinline, blue)
 circle2 = CircleAsset(250, thinline, black)
 rectangle3 = RectangleAsset(50, 600, thinline, red)
-ellipse2 = EllipseAsset(300, 100, thinline, blue)
+ellipse2 = EllipseAsset(300, 100, thinline, green)
+triangle2 = PolygonAsset([(50,50),(100,200),(500,80)], thinline, red)
+triangle3 = PolygonAsset([(89,500),(300,200),(0,80)], thinline, blue)
+triangle4 = PolygonAsset([(300,100),(100,260),(0,80)], thinline, green)
 
 #display the shapes
 Sprite(rectangle, (600, 50))
@@ -58,6 +61,9 @@ Sprite(triangle, (500,600))
 Sprite(rectangle2, (200, 50))
 Sprite(rectangle3, (500, 50))
 Sprite(ellipse2, (400, 0))
+Sprite(triangle2, (300,400))
+Sprite(triangle3, (100,450))
+Sprite(triangle4, (345,90))
 
 # add your code here /\  /\  /\
 
