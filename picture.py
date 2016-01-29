3407d85d51e6e0f7e574967932c1f002fd25f52c
haydenhatfield <haydenhatfield@hanovernorwichschools.org>
2016-01-29 14:56:05 -0500

    Updated from Brython Server: 1/29/2016 2:56:05 PM

--- picture.py
+++ picture.py
@@ -35,16 +35,17 @@ black = Color(0x000000, 1.0)
 darkgreen = Color(0x26632e, 1.0)
 nightsky = Color(0x16152b, 1.0)
 ground = Color(0x06471a, 1.0)
+wood = Color(0x542a0c, 1.0)
 line = LineStyle(1, black)
 
 
 tri = PolygonAsset([(19,72),(29,72),(25,42)], line, darkgreen )
 background = RectangleAsset( 1000, 1000, line, nightsky )
-ground = RectangleAsset( 1000, 30, line, ground )
+ground = RectangleAsset( 1000, 100, line, ground )
 
 Sprite(background, (0, 0))
 Sprite(tri, (20, 20))
-Sprite(ground, (0, 750))
+Sprite(ground, (0, 700))
 # add your code here /\  /\  /\
 
 
