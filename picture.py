420dc6018e5cbfbb06f4efd0533f5dbf59d4fd4a
haydenhatfield <haydenhatfield@hanovernorwichschools.org>
2016-02-02 14:49:44 -0500

    Updated from Brython Server: 2/2/2016 2:49:43 PM

--- picture.py
+++ picture.py
@@ -40,7 +40,7 @@ line = LineStyle(2, black)
 
 
 
-
+trunk = RectangleAsset( 20, 20, line wood)
 tri1 = PolygonAsset([(0,100),(200,100),(100,0)], line, darkgreen )
 tri2 = PolygonAsset([(0,100),(200,100),(100,0)], line, darkgreen )
 tri3 = PolygonAsset([(0,100),(200,100),(100,0)], line, darkgreen )
@@ -53,6 +53,7 @@ Sprite(tri1, (20, 550))
 Sprite(tri2, (20, 500))
 Sprite(tri3, (20, 450))
 Sprite(tri4, (20, 400))
+sprite(trunk, (20, 600))
 Sprite(ground, (0, 700))
 # add your code here /\  /\  /\
 
