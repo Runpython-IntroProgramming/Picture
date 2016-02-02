bcc86af5426bf75b0479d2463e46848d2f483f6e
haydenhatfield <haydenhatfield@hanovernorwichschools.org>
2016-02-02 14:19:06 -0500

    Updated from Brython Server: 2/2/2016 2:19:06 PM

--- picture.py
+++ picture.py
@@ -44,7 +44,7 @@ background = RectangleAsset( 1000, 1000, line, nightsky )
 ground = RectangleAsset( 1000, 100, line, ground )
 
 Sprite(background, (0, 0))
-Sprite(tri, (20, 20))
+Sprite(tri, (20, 700))
 Sprite(ground, (0, 700))
 # add your code here /\  /\  /\
 
