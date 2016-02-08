5f6a34b990b73969a09baac70c1537a12587358a
haydenhatfield <haydenhatfield@hanovernorwichschools.org>
2016-02-08 14:43:46 -0500

    Updated from Brython Server: 2/8/2016 2:43:46 PM

--- picture.py
+++ picture.py
@@ -91,7 +91,7 @@ Sprite(tri3, (20, 450))
 Sprite(tri4, (20, 400))
 Sprite(trunk, (110, 650))
 Sprite(moon, (900, 50))
-Sprite(cliff, (850, 500))
+Sprite(cliff, (850, 700))
 Sprite(ground, (0, 700))
 # add your code here /\  /\  /\
 
