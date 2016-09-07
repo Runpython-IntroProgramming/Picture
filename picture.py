15e2bddb964d3f358cc9ce45a606b7b5a6ff1b80
TheBigBlueBlob <liamszczepiorkowski@hanovernorwichschools.org>
2016-09-07 13:15:19 -0400

    Updated from Brython Server: 9/7/2016 1:15:19 PM

--- picture.py
+++ picture.py
@@ -34,12 +34,23 @@ black = Color(0x000000, 1.0)
 
 normalLine = LineStyle(1, black)
 
-rectangle = RectangleAsset(800, 200, normalLine, blue)
+ellipse = EllipseAsset(50, 150, normalLine, black)
+rectangle = RectangleAsset(700, 200, normalLine, blue)
+rectangle2 = RectangleAsset(150, 150, normalLine, blue)
+polygon = PolygonAsset([(0, 0), (300, 300), (660, 100), (0, 0)], normalLine, green)
 circle = CircleAsset(70, normalLine, red)
-Sprite(rectangle, (825, 50))
-Sprite(circle, (750, 200))
-Sprite(circle, (1100, 200))
 
+
+Sprite(rectangle2, (550, 200))
+Sprite(rectangle, (500, 50))
+Sprite(circle, (800, 200))
+Sprite(circle, (1100, 200))
+Sprite(polygon, (1100, 200))
+Sprite(polygon, (800, 200))
+Sprite(ellipse, (625, 200))
+Sprite(ellipse, (625, 300))
+Sprite(ellipse, (625, 400))
+Sprite(ellipse, (625, 500))
 
 # add your code here /\  /\  /\
 
