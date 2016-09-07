5e11487c2d02db33e9def18ee8d06ddccbad7066
DRBarnum <douglasbarnum@hanovernorwichschools.org>
2016-09-07 13:16:25 -0400

    Updated from Brython Server: 9/7/2016 1:16:22 PM

--- picture.py
+++ picture.py
@@ -32,18 +32,21 @@ green = Color(0x00ff00, 1.0)
 blue = Color(0x0000ff, 1.0)
 black = Color(0x000000, 1.0)
 yellow= Color(0xffff00, 1.0)
+brown= Color(0x8b4513, 1.0)
 # Define a line style that is a thin (1 pixel) wide black line
 thinline = LineStyle(1, black)
 rectangle = RectangleAsset (100, 100, thinline, blue)
 triangle = PolygonAsset ([(550, 500), (500, 600), (550, 600), (600, 600)], thinline, red)
 sun = EllipseAsset (100, 100, thinline, yellow)
-
+tree = CircleAsset (50, thinline, green)
+trunk = RectangleAsset (30, 50, thinline, brown)
 
 # add your code here /\  /\  /\
 Sprite (rectangle, (500, 600))
 Sprite (triangle)
 Sprite (sun)
-
+Sprite (tree, (300, 600))
+Sprite (trunk, (285, 650))
 # add your code here /\  /\  /\
 
 
