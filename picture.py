4184d03c35dce88b44be54c919acb4a76ea9426f
Aqkotz <andy.lyme@mac.com>
2016-09-12 14:00:42 -0400

    Updated from Brython Server: September 12, 2016 2:00:41 PM EDT

--- picture.py
+++ picture.py
@@ -35,15 +35,15 @@ black = Color(0x000000, 1.0)
 
 thinline = LineStyle(1, black)
 
+circlecaverock = CircleAsset (150, thinline, gray)
 rectanglesand = RectangleAsset (1000, 1000, thinline, yellow)
 polygonhouse = PolygonAsset ([(450,450), (500,450), (500,400), (475,375), (450,400)], thinline, red)
-ellipsecaveentr = EllipseAsset (50, 20, thinline, black)
-circlecaverock = CircleAsset (150, thinline, gray)
+ellipsecaveentr = EllipseAsset (75, 125, thinline, black)
 
-Sprite (rectanglesand, (300, 450))
+Sprite (circlecaverock, (900, 450))
 Sprite (polygonhouse)
 Sprite (ellipsecaveentr, (900, 450))
-Sprite (circlecaverock, (900, 450))
+Sprite (rectanglesand, (300, 450))
 # add your code here /\  /\  /\
 
 
