d80a67a92805931f88ce1d02df41151709c1ad62
T045T3R <johannestestorf@hanovernorwichschools.org>
2016-09-12 13:28:21 -0400

    Updated from Brython Server: 9/12/2016 1:28:30 PM

--- picture.py
+++ picture.py
@@ -33,11 +33,15 @@ green = Color(0x00ff00, 1.0)
 blue = Color(0x0000ff, 1.0)
 black = Color(0x000000, 1.0)
 yellow = Color(0xffff00, 1.0)
+brown = Color(0xf4a460, 1.0)
 
 thinline = LineStyle(1, black)
 
 sky=RectangleAsset(100000000000000,1000000000000, thinline ,blue)
 sun=CircleAsset(500, thinline, yellow)
+House =
+pond=
+grass= RectangleAsset(20, 10000,1000, thinline ,blue)  
 Sprite(sky)
 Sprite(sun)
 # add your code here /\  /\  /\
