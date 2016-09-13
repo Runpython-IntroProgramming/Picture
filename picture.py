7677c58b3fc1b3314a3db4f2c1ff19843fe18c40
kingmayonaise <danielmelnikov@hanovernorwichschools.org>
2016-09-13 13:56:54 -0400

    Updated from Brython Server: 9/13/2016 1:56:54 PM

--- picture.py
+++ picture.py
@@ -38,6 +38,7 @@ cloudwhite=Color(0xf5f5f5,1.0)
 glassblue=Color(0xe0ffff,1.0)
 
 thinline = LineStyle(1, black)
+noline=LineStyle(0,white)
 
 rectangleRed = RectangleAsset(170, 170, thinline, red)
 cloud=EllipseAsset(60,40,thinline,cloudwhite)
@@ -45,14 +46,20 @@ sun=CircleAsset(100,thinline,yellow)
 roof=PolygonAsset([(490,450),(680,450),(585,365),(490,450)],thinline,blue)
 window=RectangleAsset(50,50,thinline,glassblue)
 door=RectangleAsset(40,80,thinline,brown)
+rainbowBase=EllipseAsset(350,300,thinline,white)
+base=RectangleAsset(10000,1000,thinline,white)
+rBlue=EllipseAsset(370,320,thinline,blue)
+rRed=EllipseAsset(390,340,thinline,red)
+rGreen=EllipseAsset(410,360,thinline,green)
+rYellow=EllipseAsset(430,380,thinline,yellow)
 
-#rainbowBase=EllipseAssset(
-#rBlue=EllipseAssset(
-#rRed=EllipseAssset(
-#rGreen=EllipseAssset(
-#rYellow=EllipseAssset(
-#rPurple=EllipseAssset(
 
+Sprite(rYellow,(585,620))
+Sprite(rGreen,(585,620))
+Sprite(rRed,(585,620))
+Sprite(rBlue,(585,620))
+Sprite(rainbowBase,(585,620))
+Sprite(base,(0,570))
 Sprite(cloud,(200,150))
 Sprite(cloud,(700,100))
 Sprite(cloud,(400,250))
