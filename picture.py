fdb79a9226e5658d5726aa88d2dc9e6622cefe05
y0g1bear <johnwarhold@hanovernorwichschools.org>
2016-09-13 14:02:30 -0400

    Updated from Brython Server: 9/13/2016 2:02:30 PM

--- picture.py
+++ picture.py
@@ -30,14 +30,25 @@ from ggame import App, Color, LineStyle, Sprite, RectangleAsset, CircleAsset, El
 blueviolet = Color(0x8a2be2, 1.0)
 hotpink = Color(0xff69b4, 1.0)
 Aquamarine =Color(0x7fffd4, 1.0)
-
+brown = Color(0xcd5c5c, 1.0)
+green = Color(0x00ff7f, .5)
+thist = Color(0xd8bfd8, 1.0)
 thinline = LineStyle(1, hotpink)
 
 rec = RectangleAsset(169,169, thinline, Aquamarine)
 poly = PolygonAsset([(500,400), (669,400), (584.5, 350), (500, 400)], thinline, blueviolet)
+circ = CircleAsset(67, thinline, hotpink) 
+rek = RectangleAsset(50,100, thinline, brown)
+lol = CircleAsset(100, thinline, green)
+rrr = EllipseAsset(75,25, thinline, thist)
+rekt = RectangleAsset(12,24, thinline, brown)
 Sprite(rec, (500, 400))
 Sprite(poly)
-
+Sprite(circ, (67,67))
+Sprite(rek, (800,469 ))
+Sprite(lol, (825,390))
+Sprite(rrr, (500,200))
+Sprite(rekt, (444,400))
 # add your code here /\  /\  /\
 
 
