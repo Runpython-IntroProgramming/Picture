9a6920613cfe4f5de569f0009feb0a7a7e7f7482
xNimbleNavigatorx <alexandrehankel@hanovernorwichschools.org>
2016-09-19 14:00:05 -0400

    Updated from Brython Server: 9/19/2016 2:00:05 PM

--- picture.py
+++ picture.py
@@ -28,6 +28,11 @@ from ggame import App, Color, LineStyle, Sprite, RectangleAsset, CircleAsset, El
 
 # add your code here \/  \/  \/
 
+red = Color(0xff0000, 1.0)
+green = Color(0x00ff00, 1.0)
+blue = Color(0x0000ff, 1.0)
+black = Color(0x000000, 1.0)
+
 
 # add your code here /\  /\  /\
 
