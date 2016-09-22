c2464c59bdd22feb8cac500b843ba664d8581389
xNimbleNavigatorx <alexandrehankel@hanovernorwichschools.org>
2016-09-22 01:11:05 -0400

    Updated from Brython Server: 9/22/2016 1:11:02 AM

--- picture.py
+++ picture.py
@@ -35,22 +35,32 @@ black = Color(0x000000, 1.0)
 white = Color(0xffffff, 1.0)
 
 mediumline = LineStyle(5, black)
+eraserline = LineStyle(5, white)
 
 eye1 = EllipseAsset(80, 50, mediumline, white)
+eyerim = EllipseAsset(90, 65, mediumline, green)
 rectangleface = RectangleAsset(500, 500, mediumline, green)
-lip = RectangleAsset(266, 50, mediumline, red)
+lip = RectangleAsset(366, 40, mediumline, red)
 rectangel = RectangleAsset(50, 30, mediumline, white)
+pupil = CircleAsset(30, mediumline, black)
+top = PolygonAsset([(308, 96), (425, 10), (550, 60), (675, 10), (792, 96)], mediumline, green)
+chin = PolygonAsset([(700, 600), (900, 600), (900, 500)], eraserline, white)
 
-Sprite(rectangleface, (400, 200))
-Sprite(eye1, (830, 344))
-Sprite(eye1, (700, 344))
-Sprite(lip, (633, 500))
+Sprite(rectangleface, (400, 100))
+Sprite(lip, (533, 420))
+Sprite(lip, (533, 460))
 
+Sprite(eyerim, (830, 244))
+Sprite(eyerim, (700, 244))
 
+Sprite(eye1, (830, 244))
+Sprite(eye1, (700, 244))
 
+Sprite(pupil, (845, 260))
+Sprite(pupil, (715, 260))
 
-
-
+Sprite(top, (100, 0))
+Sprite(chin, (0, 0))
 
 # add your code here /\  /\  /\
 
