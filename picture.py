76f8fc59f7520880aaa7071b0a29a2c5195021f5
AverageNormalSchoolBoy <brendandufty@hanovernorwichschools.org>
2017-01-27 14:32:17 -0500

    Updated from Brython Server: 1/27/2017 2:32:16 PM

--- picture.py
+++ picture.py
@@ -27,6 +27,18 @@ for detailed information on ggame.
 from ggame import App, Color, LineStyle, Sprite, RectangleAsset, CircleAsset, EllipseAsset, PolygonAsset
 
 # add your code here \/  \/  \/
+from ggame import App, Color, LineStyle, Sprite
+from ggame import RectangleAsset, CircleAsset, EllipseAsset, PolygonAsset
+
+red = Color(0xff0000, 1.0)
+green = Color(0x00ff00, 1.0)
+blue = Color(0x0000ff, 1.0)
+black = Color(0x000000, 1.0)
+circle = CircleAsset(30, thinline, red)
+Sprite(circle, (100,100))
+
+rectangle = RectangleAsset(50, 20, thinline, blue)
+Sprite(rectangle)
 
 
 # add your code here /\  /\  /\
