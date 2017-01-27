8ddba129ba0fd09083035233b20665a57dbe75a5
AverageNormalSchoolBoy <brendandufty@hanovernorwichschools.org>
2017-01-27 14:53:57 -0500

    Updated from Brython Server: 1/27/2017 2:53:56 PM

--- picture.py
+++ picture.py
@@ -55,7 +55,7 @@ Sprite(window2, (1030, 430))
 door = CircleAsset(50, thinline, green)
 Sprite(door, (1000, 600))
 
-door` = RectangleAsset(200, 200, thinline, white)
+doo1 = RectangleAsset(200, 200, thinline, white)
 Sprite(door1, (1000, 600))
 
 
