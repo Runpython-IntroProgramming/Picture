c7bfed09bd452a2ebc6a3d59d6621e78badcb769
afeyrer <abigailfeyrer@hanovernorwichschools.org>
2017-01-30 14:27:16 -0500

    Updated from Brython Server: 1/30/2017 2:27:16 PM

--- picture.py
+++ picture.py
@@ -27,6 +27,18 @@ for detailed information on ggame.
 from ggame import App, Color, LineStyle, Sprite, RectangleAsset, CircleAsset, EllipseAsset, PolygonAsset
 
 # add your code here \/  \/  \/
+from ggame import App, Color, LineStyle, Sprite
+from ggame import RectangleAsset, CircleAsset, EllipseAsset, PolygonAsset
+
+lightblue= color(1e90ff, 1.0)
+green=color(2e8b57,1.0)
+brown=color(8b4513, .8)
+purple=color(9370db, 1.0)
+black = Color(0x000000, 1.0)
+
+line=linestyle(1,black)
+
+Petal=EllipseAsset(50,20,thinline, lightblue)
 
 
 # add your code here /\  /\  /\
