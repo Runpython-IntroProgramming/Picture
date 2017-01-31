5e618ed52bc702b7ce4b8c9eff1265d885411d93
afeyrer <abigailfeyrer@hanovernorwichschools.org>
2017-01-31 14:47:44 -0500

    Updated from Brython Server: 1/31/2017 2:47:44 PM

--- picture.py
+++ picture.py
@@ -35,6 +35,7 @@ green=Color(0x2e8b57,1.0)
 brown=Color(0x8b4513, .8)
 purple=Color(0x9370db, 1.0)
 black = Color(0x000000, 1.0)
+blue=Color(0x0000f,1.0)
 
 line=LineStyle(1,black)
 
@@ -42,15 +43,22 @@ Petal=EllipseAsset(50,20,line, lightblue)
 petal2=EllipseAsset(20,50,line, lightblue)
 Circle=CircleAsset(30, line, purple)
 stem=RectangleAsset(5,100,line,green)
+pot=PolygonAsset([(300,500), (500,500), (450,600), (350,600), (300,500)], line, brown)
 
 Sprite(Circle, (375,300))
 Sprite(Petal, (300,300))
 Sprite(Petal, (450,300))
 Sprite(petal2, (380,225))
-Sprite(stem, (380,325))
-Sprite(petal2, (380,325))
-
-
+Sprite(stem, (380,400))
+Sprite(petal2, (380,375))
+petal3=Sprite(petal2,(315,245))
+petal3.rotation=3.14/4
+petal4=Sprite(petal2,(430,355))
+petal4.rotation=3.14/4
+petal5=Sprite(petal,(430,245))
+petal5.rotation=3.14/4
+petal6=Sprite(petal,(315,355))
+petal6.rotation=3.14/4
 
 # add your code here /\  /\  /\
 
