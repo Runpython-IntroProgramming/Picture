2e3db7ba591e43a2ab7f40f270060569a4b6e5d4
Jetzal <Jetzal@users.noreply.github.com>
2017-02-01 13:44:06 -0500

    Updated from Brython Server: 2/1/2017 1:44:05 PM

--- picture.py
+++ picture.py
@@ -35,6 +35,7 @@ blue = Color(0x0000ff, 1.0)
 black = Color(0x000000, 1.0)
 brown = Color(0x633C1F, 1.0)
 white = Color(0xFFFFFF, 1.0)
+yellow=Color(0xFFFF00, 1.0)
 
 thinline = LineStyle(1, black)
 House = RectangleAsset(700, 700, thinline, brown)
@@ -44,6 +45,9 @@ ellipse=EllipseAsset(30, 20, thinline, white)
 polygon=PolygonAsset([(0,0), (700,0), (350,-300), (0,0)], thinline, black)
 door= RectangleAsset(100,200, thinline, black)
 chimmney= RectangleAsset(70, 170, thinline, black)
+Sun=CircleAsset(130, thinline, yellow)
+Earth=RectangleAsset(10010, 300, thinline, green)
+
 
 Sprite(House,(500,400))
 Sprite(polygon,(500,400))
@@ -53,8 +57,8 @@ Sprite(window, (1050, 550))
 Sprite(door, (800, 680))
 Sprite(ellipse,(850,705))
 Sprite(chimmney, (600, 200))
-
-
+Sprite(Sun, (80, 80))
+Sprite(Earth,(-20, 830))
 # add your code here /\  /\  /\
 
 
