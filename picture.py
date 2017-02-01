e8f5c136b36ea07682c5005f60404799c7263737
Jetzal <Jetzal@users.noreply.github.com>
2017-02-01 13:59:06 -0500

    Updated from Brython Server: 2/1/2017 1:59:06 PM

--- picture.py
+++ picture.py
@@ -24,7 +24,7 @@ http://brythonserver.github.io/ggame/
 for detailed information on ggame.
 
 """
-from ggame import App, Color, LineStyle, Sprite, RectangleAsset, CircleAsset, EllipseAsset, PolygonAsset
+#from ggame import App, Color, LineStyle, Sprite, RectangleAsset, CircleAsset, EllipseAsset, PolygonAsset
 
 # add your code here \/  \/  \/
 
