a4d8775f7897ddf013a6d0d673610719a9d3d22c
Amovi <anthony.movizzo@outlook.com>
2017-02-07 14:39:22 -0500

    Update picture.py

--- picture.py
+++ picture.py
@@ -1,36 +1,32 @@
-"""
-picture.py
-Author: <your name here>
-Credit: <list sources used, if any>
-
-Assignment:
-
-Use the ggame library to "paint" a graphical picture of something (e.g. a house, a face or landscape).
-
-Use at least:
-1. Three different Color objects.
-2. Ten different Sprite objects.
-3. One (or more) RectangleAsset objects.
-4. One (or more) CircleAsset objects.
-5. One (or more) EllipseAsset objects.
-6. One (or more) PolygonAsset objects.
-
-See:
-https://github.com/HHS-IntroProgramming/Standards-and-Syllabus/wiki/Displaying-Graphics
-for general information on how to use ggame.
-
-See:
-http://brythonserver.github.io/ggame/
-for detailed information on ggame.
-
-"""
-from ggame import App, Color, LineStyle, Sprite, RectangleAsset, CircleAsset, EllipseAsset, PolygonAsset
-
-# add your code here \/  \/  \/
-
-
-# add your code here /\  /\  /\
-
-
+from ggame import App, Color, LineStyle, Sprite
+from ggame import RectangleAsset, CircleAsset, EllipseAsset, PolygonAsset
+red = Color(0xff0000, 1.0)
+green = Color(0x00ff00, 1.0)
+blue = Color(0x0000ff, 1.0)
+black = Color(0x000000, 1.0)
+thinline = LineStyle(1, black)
+rectangle = RectangleAsset(120, 300, thinline, blue)
+Sprite(rectangle)
 myapp = App()
 myapp.run()
+
+Polygon = PolygonAsset ([(0,0),(40,-10),(80,0),(40,50),(0,0)] ,thinline,green)
+Sprite(Polygon,(30,30))
+
+Ellipse = EllipseAsset(40,40, thinline, black)
+Sprite(Ellipse,(10,150))
+
+Circle = CircleAsset(30, thinline, red)
+Sprite(Circle,(120,50))
+
+Circle = CircleAsset(30, thinline, red)
+Sprite(Circle,(120,120))
+
+Circle = CircleAsset(30, thinline, red)
+Sprite(Circle,(120,190))
+
+Circle = CircleAsset(30, thinline, red)
+Sprite(Circle,(120,260))
+
+rectangle = RectangleAsset(10, 400, thinline, blue)
+Sprite(rectangle)
