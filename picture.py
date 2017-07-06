4178ff8ba1188ae7936bbcaf003a4842bcea6a8f
smeds1 <smeds1@users.noreply.github.com>
2017-07-06 10:38:42 -0400

    Updated from Brython Server: 7/6/2017 10:38:41 AM

--- picture.py
+++ picture.py
@@ -28,7 +28,27 @@ from ggame import App, Color, LineStyle, Sprite, RectangleAsset, CircleAsset, El
 
 red = Color(0xff0000, 1.0)
 green = Color(0x00ff00, 1.0)
-blue = color(0x0000ff, 1.0)
+blue = Color(0x0000ff, 1.0)
+white = Color(0xffffff, 1.0)
+black = Color(0x000000, 1.0)
+
+blackLine = LineStyle(1,black)
+
+outline = RectangleAsset(600,400,blackLine,white)
+circle = CircleAsset(50,blackLine,red)
+ellipse = EllipseAsset(20,40,blackLine,green)
+polygon = PolygonAsset([(20,30),(150,60),(190,200)],blackLine,blue)
+
+Sprite(outline)
+Sprite(circle,(100,200))
+Sprite(ellipse,(300,300))
+Sprite(polygon)
+Sprite(circle,(100,400))
+Sprite(ellipse,(400,200))
+Sprite(polygon,(100,20))
+Sprite(circle,(600,200))
+Sprite(ellipse,(700,400))
+Sprite(polygon,(500,500))
 
 
 myapp = App()
