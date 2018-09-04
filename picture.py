fff513f9776dee5226fc19e4add4f436b246fdd3
ericgoodney <42844640+ericgoodney@users.noreply.github.com>
2018-09-04 13:05:43 -0400

    Updated from Brython Server: 9/4/2018 1:05:44 PM

--- picture.py
+++ picture.py
@@ -33,9 +33,10 @@ blue = Color(0x000ff, 1.0)
 black = Color(0x000000, 1.0)
 
 thinline = LineStyle(1, black)
-rectangle = RectangleAsset(50,20, thinline, blue)
+rectangle = RectangleAsset(60,60, thinline, blue)
 
 Sprite(rectangle)
+
 
 # add your code here /\  /\  /\
 
