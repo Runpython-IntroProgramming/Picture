dbc0cc88b621d211417cc3ca37c85d52b328317b
hackmeehan <42844639+hackmeehan@users.noreply.github.com>
2018-09-04 19:53:28 -0400

    Updated from Brython Server: 9/4/2018 7:53:28 PM

--- picture.py
+++ picture.py
@@ -31,6 +31,7 @@ from ggame import App, Color, LineStyle, Sprite, RectangleAsset, CircleAsset, El
 green = Color(0x00ff00, 1.0)
 blue = Color(0x0f0fff, 1.0)
 black = Color(0x000000, 1.0)
+white = Color(0xffffff, 1.0)
 thinline = LineStyle(2, blue)
 rectangle = RectangleAsset(200, 100, thinline, green)
 Sprite(rectangle, (200, 200))
@@ -38,8 +39,18 @@ Sprite(rectangle, (200, 200))
 roof = PolygonAsset([(305, 100), (210, 150), (407, 150)], thinline, black)
 Sprite(roof, (200, 150))
 
-door = RectangleAsset(10, 20, thinline, blue)
-Sprite(door, (200, 200))
+door = RectangleAsset(30, 60, thinline, blue)
+Sprite(door, (255, 240))
+
+doorwindow = RectangleAsset(29, 22, thinline, black)
+Sprite(doorwindow, (255, 242))
+
+window = CircleAsset(22, thinline, black)
+Sprite(window, (318, 230))
+
+path = PolygonAsset([(310, 300), (260, 360), (250, 360), (300, 300)], thinline, black)
+Sprite(path, (300, 300))
+
 # add your code here /\  /\  /\
 
 
