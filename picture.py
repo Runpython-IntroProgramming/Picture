b4708872d420ed608ecfd754c5a4a1afb2caf2a6
katherinenaughton <42843645+katherinenaughton@users.noreply.github.com>
2018-09-05 11:44:28 -0400

    Updated from Brython Server: 9/5/2018 11:44:27 AM

--- picture.py
+++ picture.py
@@ -1,7 +1,7 @@
 """
 picture.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: Katie Naughton
+Credit: I worked alone. 
 
 Assignment:
 
@@ -26,10 +26,14 @@ for detailed information on ggame.
 """
 from ggame import App, Color, LineStyle, Sprite, RectangleAsset, CircleAsset, EllipseAsset, PolygonAsset
 
-# add your code here \/  \/  \/
+# 4 colors, no transparency
+red = Color(0xff0000, 1.0)
+green = Color(0x00ff00, 1.0)
+blue = Color(0x0000ff, 1.0)
+black = Color(0x000000, 1.0)
 
 
-# add your code here /\  /\  /\
+
 
 
 myapp = App()
