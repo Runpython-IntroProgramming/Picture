477b9583b1d5603fa479279d86496aacae5b874b
NedrowNomae <42844326+NedrowNomae@users.noreply.github.com>
2018-09-05 12:25:57 -0400

    Updated from Brython Server: 9/5/2018 12:25:57 PM

--- picture.py
+++ picture.py
@@ -30,8 +30,9 @@ red = Color(0xff0000, 1.0)
 green = Color(0x00ff00, 1.0)
 blue = Color(0x0000ff, 1.0)
 black = Color(0x000000, 1.0)
-thinline = LineStyle(5, black)
+thinline = LineStyle(1, black)
 rectangle = RectangleAsset(50, 20, thinline, blue)
+Sprite(rectangle)
 # add your code here \/  \/  \/
 
 #Sprite(CircleAsset("Small-mariogood2.png")
