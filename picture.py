85affeda88aec7f8e5baaf70695b332a8e3d79cc
NedrowNomae <42844326+NedrowNomae@users.noreply.github.com>
2018-09-05 12:25:20 -0400

    Updated from Brython Server: 9/5/2018 12:25:20 PM

--- picture.py
+++ picture.py
@@ -26,6 +26,12 @@ for detailed information on ggame.
 """
 from ggame import App, Color, LineStyle, Sprite, RectangleAsset, CircleAsset, EllipseAsset, PolygonAsset, ImageAsset
 
+red = Color(0xff0000, 1.0)
+green = Color(0x00ff00, 1.0)
+blue = Color(0x0000ff, 1.0)
+black = Color(0x000000, 1.0)
+thinline = LineStyle(5, black)
+rectangle = RectangleAsset(50, 20, thinline, blue)
 # add your code here \/  \/  \/
 
 #Sprite(CircleAsset("Small-mariogood2.png")
