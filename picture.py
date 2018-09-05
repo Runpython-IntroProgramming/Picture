08d724f825cc6bc907a4d5a63cb71dd8bf43e1d3
NedrowNomae <42844326+NedrowNomae@users.noreply.github.com>
2018-09-05 12:12:47 -0400

    Updated from Brython Server: 9/5/2018 12:12:47 PM

--- picture.py
+++ picture.py
@@ -30,7 +30,7 @@ for detailed information on ggame.
 
 from ggame import App, ImageAsset, Sprite
 # Create a displayed object at 100,100 using an image asset
-Sprite(ImageAsset("Small-mario.png"), (100,100))
+Sprite(ImageAsset("Small-mariogood.png"), (100,100))
 # Create the app, with a 500x500 pixel stage
 app = App(500,500)  
 # Run the app
