691f0e0c9d81c0b7cfa797d777341976529292b2
ericgoodney <42844640+ericgoodney@users.noreply.github.com>
2018-09-05 14:20:59 -0400

    Updated from Brython Server: 9/5/2018 2:20:59 PM

--- picture.py
+++ picture.py
@@ -27,30 +27,31 @@ for detailed information on ggame.
 from ggame import App, Color, LineStyle, Sprite, RectangleAsset, CircleAsset, EllipseAsset, PolygonAsset
 
 # add your code here \/  \/  \/
-mint = Color(0x00C957, 1.0) 
-green = Color(0x1C86EE, 1.0)
+mintgreen = Color(0x00C957, 1.0) 
+
 brown = Color(0xCD5B45, 1.0)
 black = Color(0x000000, 1.0)
 drkgreen= Color(0x006400, 1.0)
+houseclr= Color(0xFFF8DC, 1.0)
 
-thinline = LineStyle(1, black)
-rectangle = RectangleAsset(250,250, thinline, mint)
-triangle = PolygonAsset([(200,0), (325,-100),  (450,0)], thinline, brown)
-rectangle2 = RectangleAsset(50,100, thinline, black)
-window1 = RectangleAsset(40,90, thinline, mint)
-window2 = RectangleAsset(40,90, thinline, mint)
-thing1 = EllipseAsset(40,40, thinline, green)
-thing2 = CircleAsset(25, thinline, black)
+thinline = LineStyle(1, brown)
+
+rectangle = RectangleAsset(250,250, thinline, houseclr)
+triangle = PolygonAsset([(200,0), (325,-100),  (450,0)], thinline, black)
+rectangle2 = RectangleAsset(50,100, thinline, brown)
+window1 = RectangleAsset(60,60, thinline, mintgreen)
+window2 = RectangleAsset(60,60, thinline, mintgreen)
+circlewindow = CircleAsset(25, thinline, brown)
 tree1 = PolygonAsset([(100,0), (150,-100),  (200,0)], thinline, drkgreen)
-tree2 
+
 
 Sprite(rectangle,(400,200))
 Sprite(triangle,(400,100))
 Sprite(rectangle2,(500,350))
 Sprite(window1,(430,210))
 Sprite(window2,(580,210))
-Sprite(thing1,(485,210))
-Sprite(thing2,(500,140))
+
+Sprite(circlewindow,(500,140))
 Sprite(tree1,(300,350))
 
 # add your code here /\  /\  /\
