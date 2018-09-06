9b7cec937ba806380a01da822401939e21f7e9b6
NedrowNomae <42844326+NedrowNomae@users.noreply.github.com>
2018-09-06 12:41:14 -0400

    Updated from Brython Server: 9/6/2018 12:41:14 PM

--- picture.py
+++ picture.py
@@ -38,8 +38,8 @@ rectangle = RectangleAsset(500, 20, thinline, brown)
 Sprite((rectangle), (0, 220))
 pipe1 = RectangleAsset(30, 50, thinline, green)
 Sprite((pipe1), (350, 180))
-pipe2 = EllipseAsset(60, 20, thickgreen, black)
-Sprite((pipe2), (320, 150))
+pipe2 = EllipseAsset(40, 20, thickgreen, black)
+Sprite((pipe2), (330, 150))
 # add your code here \/  \/  \/
 
 
