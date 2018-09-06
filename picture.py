aac8f5b13c286e357795a2b3ba29f7cad2a89052
NedrowNomae <42844326+NedrowNomae@users.noreply.github.com>
2018-09-06 13:07:51 -0400

    Updated from Brython Server: 9/6/2018 1:07:50 PM

--- picture.py
+++ picture.py
@@ -26,7 +26,7 @@ for detailed information on ggame.
 """
 from ggame import App, Color, LineStyle, Sprite, RectangleAsset, CircleAsset, EllipseAsset, PolygonAsset, ImageAsset
 
-white = Color(0x000000, 1.0)
+white = Color(0xffffff, 1.0)
 red = Color(0xff0000, 1.0)
 green = Color(0x00ff00, 1.0)
 blue = Color(0x0000ff, 1.0)
