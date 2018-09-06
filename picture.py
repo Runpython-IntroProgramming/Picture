ce5f532faeb35db42f8f4156ac5bbc2888b9794e
daleyhacker <42852983+daleyhacker@users.noreply.github.com>
2018-09-06 11:12:55 -0400

    Updated from Brython Server: 9/6/2018 11:12:55 AM

--- picture.py
+++ picture.py
@@ -30,14 +30,40 @@ from ggame import App, Color, LineStyle, Sprite, RectangleAsset, CircleAsset, El
 from ggame import App, Color, LineStyle, Sprite
 black = Color(0x000000, 1.0)
 brown = Color(0x8B4513, 1.0)
+black1 = Color(0x000000, 0.2)
 thinline = LineStyle(1, black)
+#House bottom
 rectangle = RectangleAsset(500, 500, thinline, brown)
 Sprite(rectangle, (250, 250))
-
+#House top
 triangle = PolygonAsset([(-250, 250), (0,0), (250, 250)], thinline, brown)
 Sprite( triangle, (250, 0))
-#poly = PolygonAsset([(250, 250), (100, 100), (100, 250)], thinline, black)
-#Sprite(poly, (250,250))
+
+#Windows Top
+window = RectangleAsset(50, 75, thinline, black1)
+Sprite( window, (295, 300))
+
+window2 = RectangleAsset(50, 75, thinline, black1)
+Sprite( window2, (475, 300))
+
+window3 = RectangleAsset(50, 75, thinline, black1)
+Sprite( window3, (655, 300))
+
+#Windows bottom
+window4 = RectangleAsset(50, 75, thinline, black1)
+Sprite( window4, (295, 425))
+
+window5 = RectangleAsset(50, 75, thinline, black1)
+Sprite( window5, (655, 425))
+
+#Door
+door = RectangleAsset(50, 125, thinline, black)
+Sprite( door, (475, 425))
+
+doorhandle = CircleAsset(5, thinline, brown)
+Sprite( doorhandle, (510, 480))
+
+#Window Circle
 circle =  CircleAsset(50, thinline, black)
 Sprite(circle, (450, 30))
 
