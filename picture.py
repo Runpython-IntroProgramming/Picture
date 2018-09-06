4b16adadfe7e2d399f38675c02cc7d372661d069
NedrowNomae <42844326+NedrowNomae@users.noreply.github.com>
2018-09-06 12:37:45 -0400

    Updated from Brython Server: 9/6/2018 12:37:44 PM

--- picture.py
+++ picture.py
@@ -35,7 +35,7 @@ thinline = LineStyle(1, black)
 medline = LineStyle(2, black)
 rectangle = RectangleAsset(500, 20, thinline, brown)
 Sprite((rectangle), (0, 220))
-pipe1 = RectangleAsset
+pipe1 = RectangleAsset(30, 50, thinline, green)
 # add your code here \/  \/  \/
 
 
