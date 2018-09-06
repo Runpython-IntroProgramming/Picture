745df831772b8164a2451db58543cc33e57f8687
NedrowNomae <42844326+NedrowNomae@users.noreply.github.com>
2018-09-06 13:03:06 -0400

    Updated from Brython Server: 9/6/2018 1:03:05 PM

--- picture.py
+++ picture.py
@@ -46,7 +46,7 @@ block = RectangleAsset(15, 15, thinline, brown)
 Sprite((block), (200, 100))
 Sprite((block), (230, 100))
 Sprite((block), (215, 100))
-goomba = PolygonAsset([(15, 0), (-7.5, 15), (15, 0), (100, 100)], thinbrown, brown)
+goomba = PolygonAsset([(15, 0), (-7.5, 15), (15, 0)], thinbrown, brown)
 Sprite((goomba), (50, 50))
 # add your code here \/  \/  \/
 
