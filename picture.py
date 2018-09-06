db1fcb6c4740fd7b697b2a13091b061bfdcd828c
pierremayo <42851003+pierremayo@users.noreply.github.com>
2018-09-06 11:30:54 -0400

    Updated from Brython Server: 9/6/2018 11:30:54 AM

--- picture.py
+++ picture.py
@@ -27,6 +27,57 @@ for detailed information on ggame.
 from ggame import App, Color, LineStyle, Sprite, RectangleAsset, CircleAsset, EllipseAsset, PolygonAsset
 
 # add your code here \/  \/  \/
+Tan = Color(0xFFEBCD, 1.0)
+White = Color(0xFFFFFF, 1.0)
+DarkBlue = Color(0x00008B, 1.0)
+Red = Color(0xff0000, 1.0)
+Black = Color(0x000000, 1.0)
+Gold = Color(0xFFB90F, 1.0)
+
+thinline = LineStyle(1, Black)
+thickline = LineStyle(2,Black)
+
+Head=RectangleAsset(82, 80, thinline, Tan)
+Eye1=RectangleAsset(15,20, thinline, White)
+Eye2=RectangleAsset(5, 10, thinline, Black)
+Mouth=EllipseAsset(20,10, thinline, Black)
+Tongue=EllipseAsset(8,5, thinline, Red)
+Hair=RectangleAsset(2,8, thinline, Gold)
+Body=PolygonAsset([(80,80),(80,220),(160,220),(160,80)], thinline, DarkBlue)
+Pocket=RectangleAsset(20,20, thickline, DarkBlue)
+
+
+Sprite(Head, (80,80))
+Sprite(Eye1, (95, 100))
+Sprite(Eye1, (130, 100))
+Sprite(Eye2, (95,100))
+Sprite(Eye2, (140,110))
+Sprite(Mouth, (100,130))
+Sprite(Tongue, (113,140))
+Sprite(Hair, (80,75))
+Sprite(Hair, (85,75))
+Sprite(Hair, (90,75))
+Sprite(Hair, (95,75))
+Sprite(Hair, (100,75))
+Sprite(Hair, (105,75))
+Sprite(Hair, (110,75))
+Sprite(Hair, (115,75))
+Sprite(Hair, (120,75))
+Sprite(Hair, (125,75))
+Sprite(Hair, (130,75))
+Sprite(Hair, (135,75))
+Sprite(Hair, (140,75))
+Sprite(Hair, (145,75))
+Sprite(Hair, (150,75))
+Sprite(Hair, (155,75))
+Sprite(Hair, (160,75))
+Sprite(Body, (80,160))
+Sprite(Pocket, (130,180))
+
+
+
+
+
 
 
 # add your code here /\  /\  /\
