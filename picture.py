56efdc438879a9a5c9dbf5fd2865541a32ce66b7
NedrowNomae <42844326+NedrowNomae@users.noreply.github.com>
2018-09-06 12:31:05 -0400

    Updated from Brython Server: 9/6/2018 12:31:04 PM

--- picture.py
+++ picture.py
@@ -30,10 +30,10 @@ red = Color(0xff0000, 1.0)
 green = Color(0x00ff00, 1.0)
 blue = Color(0x0000ff, 1.0)
 black = Color(0x000000, 1.0)
-brown = color(A52A2A, 1.0)
+brown = color(0xA52A2A, 1.0)
 thinline = LineStyle(1, black)
 medline = LineStyle(2, black)
-rectangle = RectangleAsset(50, 20, thinline, blue)
+rectangle = RectangleAsset(50, 20, thinline, brown)
 Sprite((rectangle), (400, 220))
 # add your code here \/  \/  \/
 
