6b21b3ba3ad1cc7f5f30efa59cf044d9b21bf568
mgardner3901 <31485789+mgardner3901@users.noreply.github.com>
2018-09-07 12:37:16 -0400

    Updated from Brython Server: 9/7/2018 12:37:15 PM

--- picture.py
+++ picture.py
@@ -28,7 +28,16 @@ from ggame import App, Color, LineStyle, Sprite, RectangleAsset, CircleAsset, El
 
 # add your code here \/  \/  \/
 
+red = Color(0xff0000, 1.0)
+blue = Color(0x0000ff, 1.0)
+green = Color(0x00ff00, 1.0)
+black = Color(0x000000, 1.0)
 
+line = LineStyle(3, black)
+
+rectangle = RectangleAsset(100, 50, line, blue)
+
+Sprite(rectangle)
 
 
 
