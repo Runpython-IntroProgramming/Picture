7b4376576a849014c984a024abc967adb77c89c6
daleyhacker <42852983+daleyhacker@users.noreply.github.com>
2018-09-11 12:11:25 -0400

    Updated from Brython Server: 9/11/2018 12:11:25 PM

--- picture.py
+++ picture.py
@@ -37,27 +37,22 @@ red = Color(0xff0000, 1.0)
 green = Color(0x00ff00, 1.0)
 blue = Color(0x0000ff, 1.0)
 black = Color(0x000000, 1.0)
-
+"Sine Graph"
 import math
 thinline = LineStyle(1, black)
 sincircle = CircleAsset(5, thinline, red)
 xcoordinates = range(0, 360, 10)
-sprites = [Sprite(sincircle, ((x), 100*sin(x*10) + 100)) for x in xcoordinates]
+#(100+100*sin(radians(x)))
+sprites = [Sprite(sincircle, ((x), 100+100*sin((x*10)))) for x in xcoordinates]
 
+
+
+"Cosine Graph"
 import math
 thinline = LineStyle(1, black)
 coscircle = CircleAsset(5, thinline, blue)
-
 sprites = [Sprite(coscircle, ((x), 100*cos(x*10) + 100)) for x in xcoordinates]
 
-"""
-thinline = LineStyle(1, black)
-mycircle = CircleAsset(5, thinline, blue)
-xcoordinates = range(150, 500, 20)
-
-# Generate a list of sprites that form a line!
-sprites = [Sprite(mycircle, (x, x*0.5 + 100)) for x in xcoordinates]
-"""
 
 myapp = App()
 myapp.run()
