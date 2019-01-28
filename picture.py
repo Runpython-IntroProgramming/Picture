a1bc6378ae9b242d773bd4bacf72748cf76a958e
waSclthu11 <46973645+waSclthu11@users.noreply.github.com>
2019-01-28 13:05:23 -0500

    Updated from Brython Server: 1/28/2019 1:05:22 PM

--- picture.py
+++ picture.py
@@ -31,8 +31,10 @@ wah = Color(0xedcc9b, 1.0)
 purple= Color(0xce27d3, 1.0)
 white = Color(0xffffff, 1.0)
 black = Color(0x000000, 1.0)
+blue = Color(0x00ffff, 1.0)
 thinline = LineStyle(1, black)
 
+eyeshadow = EllipseAsset(25, 15, thinline, blue)
 eye = EllipseAsset(25, 15, thinline, white)
 face = EllipseAsset(100, 100, thinline, wah)
 rectangle = RectangleAsset(50, 20, thinline, purple)
@@ -40,10 +42,12 @@ hat = EllipseAsset(100, 70, thinline, purple)
 triangle = PolygonAsset([(100,100), (50, 60), (200,70)], thinline, wah)
 
 Sprite(face, (289, 120))
-Sprite(rectangle, (200, 60))
+Sprite(eyeshadow, (305, 230))
+Sprite(eye, (305, 225))
+Sprite(eye, (420, 225))
 Sprite(hat, (290, 100))
-Sprite(eye, (290, 150))
-Sprite(eye, (350, 150))
+Sprite(rectangle, (200, 60))
+
 
 
 # add your code here /\  /\  /\
