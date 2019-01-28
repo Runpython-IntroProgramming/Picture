b0598d2de6f431367e5fb45f1d71d22594d20033
miViriaz15 <46974607+miViriaz15@users.noreply.github.com>
2019-01-28 15:03:22 -0500

    Updated from Brython Server: 1/28/2019 3:03:21 PM

--- picture.py
+++ picture.py
@@ -40,13 +40,14 @@ thinlineblack = LineStyle(1, black)
 #Defining Shapes
 triangle = PolygonAsset([(500,500), (1000,500), (750, 500+ 250*sqrt(3))],thinlineblue, cornflowerblue)
 circle = CircleAsset(100,thinlineblue,cornflowerblue)
-#smalltriangle = PolygonAsset(
+smalltriangle = PolygonAsset([(725,600), (775,600), (750, 500+ 25*sqrt(3))],thinlineblack, black)
+
 #print
 s=Sprite(triangle, (120,120))
 s.scale=0.8
 Sprite(circle, (60,45))
 Sprite(circle, (380,45))
-
+Sprite(smalltriangle, (120,120))
 #s.rotation=inradians
 # add your code here /\  /\  /\
 
