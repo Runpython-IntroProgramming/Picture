9e3a29cae050673dd5743d66cab83efcfeef63d9
raLiinu30 <46948627+raLiinu30@users.noreply.github.com>
2019-01-29 14:57:00 -0500

    Updated from Brython Server: 1/29/2019 2:56:58 PM

--- picture.py
+++ picture.py
@@ -27,7 +27,23 @@ for detailed information on ggame.
 from ggame import App, Color, LineStyle, Sprite, RectangleAsset, CircleAsset, EllipseAsset, PolygonAsset
 
 # add your code here \/  \/  \/
+lightblue = Color(0xBFEFFF,0.8)
+brown = Color(0x8B4726, 0.9)
+grey = Color(0xCDC9C9, 1.0)
+black = Color(0x000000, 1.0)
 
+thinline = LineStyle(1, black)
+
+tank = RectangleAsset(900,600, thinline, lightblue)
+pebbles = RectangleAsset(900, 45, thinline, brown)
+castle = RectangleAsset(350, 200, thinline, grey)
+castletop = PolygonAsset([(40,60), (127.5,-75), (215,60)],thinline, grey)
+tower = RectangleAsset(325, 200, thinline, grey)
+
+Sprite(tank, (100,110))
+Sprite(pebbles, (100,665))
+Sprite(castle, (600,475))
+Sprite(castletop, (600,195))
 
 # add your code here /\  /\  /\
 
