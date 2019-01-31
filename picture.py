dde56736646df54135e015a3af12962e875a3baa
raLiinu30 <46948627+raLiinu30@users.noreply.github.com>
2019-01-31 14:49:42 -0500

    Updated from Brython Server: 1/31/2019 2:49:41 PM

--- picture.py
+++ picture.py
@@ -39,6 +39,7 @@ orange = Color(0xF5920F, 1.0)
 darkorange = Color(0xD07702, 1.0)
 darkorange1 = Color(0xD07702, 0)
 blue = Color(0x0285D0, 1.0)
+white = Color(0xFFFFFF, 1.0)
 
 
 thinline = LineStyle(1, grey)
@@ -46,6 +47,7 @@ thinline1 = LineStyle(1, brown)
 thinline2 = LineStyle(1, black1)
 thinline3 = LineStyle(1, green)
 thinline4 = LineStyle(1, darkorange)
+thinline5 = LineStyle(1, darkorange1)
 
 tank = RectangleAsset(900,600, thinline2, lightblue)
 pebbles = RectangleAsset(900, 45, thinline2, tan)
@@ -71,9 +73,10 @@ fish = EllipseAsset(45, 27, thinline4, orange)
 tail = EllipseAsset(20,10, thinline4, darkorange)
 fins = EllipseAsset(15,7, thinline4, darkorange)
 fish2 = RectangleAsset(80, 80, thinline3, green)
-tail2 = PolygonAsset([(40,40), (70,20), (70,60)],thinline4, darkorange)
+tail2 = PolygonAsset([(40,40), (70,20), (70,60)],thinline4, blue)
 line1 = PolygonAsset([(40,60), (40,12), (40,60)],thinline2, black)
-eye = EllipseAsset(3,3, thinline2, black)
+eye = EllipseAsset(2,2, thinline5, black)
+eye1 = EllipseAsset(5,5, thinline5, white)
 
 Sprite(tank, (100,110))
 Sprite(pebbles, (100,665))
@@ -153,7 +156,9 @@ e.rotation = 2.35
 f = Sprite(tail2, (380,389))
 f.rotation = 3.12
 Sprite(line1, (215,202))
-Sprite(eye, (200,215))
+Sprite(eye1, (200,213))
+Sprite(eye, (201.5,216))
+
 
 
 # add your code here /\  /\  /\
